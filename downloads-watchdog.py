fbfa4bc3fab864beffb49eedd856d3289a6d14b5
rayct <rayct@icloud.com>
2021-05-29 16:22:42 +0100

    Imports and file mapping array

--- downloads-watchdog.py
+++ downloads-watchdog.py
@@ -1,1 +1,25 @@
 # Python-Watchdog
+import os
+import time
+import sys
+
+from watchdog.observers
+import Observer
+from watchdog.events
+import FileSystemEventHandler
+
+folder_to_monitor = sys.argv[1]
+
+file_folder_mapping = {
+    '.png':'images',
+    '.png':'images',
+    '.png':'images',
+    '.png':'images',
+    '.png':'images',
+    '.png':'images',
+    '.png':'images',
+    '.png':'images',
+}
+
+class
+DownloadFileHandler()
